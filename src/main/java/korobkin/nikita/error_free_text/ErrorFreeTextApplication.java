523ad4202aebfe6622d26775047a89b25c5dbc2e
coldsteeze <nikitaproduction@mail.ru>
2026-07-13 18:28:24 +0300

    chore: initialize project structure

---
+++ src/main/java/korobkin/nikita/error_free_text/ErrorFreeTextApplication.java
@@ -0,0 +1,13 @@
+package korobkin.nikita.error_free_text;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorFreeTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorFreeTextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/korobkin/nikita/error_free_text/ErrorFreeTextApplicationTests.java
@@ -0,0 +1,13 @@
+package korobkin.nikita.error_free_text;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorFreeTextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
